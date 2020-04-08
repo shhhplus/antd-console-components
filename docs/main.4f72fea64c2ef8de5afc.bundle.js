201101261ac9e58de0e37fb6cf615f2fe80c90c1
shhhplus <shhhplus@msn.com>
2020-04-08 23:08:26 +0800

    build storybook

---
+++ docs/main.4f72fea64c2ef8de5afc.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{500:function(n,o,p){p(501),p(644),p(1392),p(1397),n.exports=p(1453)},563:function(n,o){}},[[500,1,2]]]);
---
+++ docs/precache-manifest.bef7b9f12b2e6e23cd8a2fb8f229839f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77b224f8dbcc0b4a596a92117e531f5",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.fadf2f48a45798d5df1c.bundle.js"
+  },
+  {
+    "url": "runtime~main.fadf2f48a45798d5df1c.bundle.js"
+  },
+  {
+    "revision": "5227332f30b924577a8c",
+    "url": "static/css/main.d1a88521.chunk.css"
+  },
+  {
+    "revision": "45b9df5e122934ccd0cedde5fcb7b4a3",
+    "url": "static/media/icon-react.45b9df5e.svg"
+  },
+  {
+    "url": "vendors~main.fadf2f48a45798d5df1c.bundle.js"
+  },
+  {
+    "url": "vendors~main.fadf2f48a45798d5df1c.bundle.js.LICENSE.txt"
+  }
+]);
