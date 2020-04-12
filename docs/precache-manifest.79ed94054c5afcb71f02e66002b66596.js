fc67518d836964d332b437033544428b9f6a10d0
shhhplus <shhhplus@msn.com>
2020-04-12 16:08:28 +0800

    build storybook

---
+++ docs/precache-manifest.79ed94054c5afcb71f02e66002b66596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3c1f4c93659d761ccd7de964ce690e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ea2885ca29a0903c7237.bundle.js"
+  },
+  {
+    "url": "runtime~main.ea2885ca29a0903c7237.bundle.js"
+  },
+  {
+    "revision": "77c3fbc81dcb527bdb9d",
+    "url": "static/css/main.be4791ff.chunk.css"
+  },
+  {
+    "revision": "45b9df5e122934ccd0cedde5fcb7b4a3",
+    "url": "static/media/icon-react.45b9df5e.svg"
+  },
+  {
+    "url": "vendors~main.ea2885ca29a0903c7237.bundle.js"
+  },
+  {
+    "url": "vendors~main.ea2885ca29a0903c7237.bundle.js.LICENSE.txt"
+  }
+]);
