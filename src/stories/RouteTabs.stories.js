a4240166a58d1db3f07a5c2772d07e10685adca8
shhhplus <shhhplus@msn.com>
2020-04-16 12:23:48 +0800

    update RouteTabs stories

--- src/stories/RouteTabs.stories.js
+++ src/stories/RouteTabs.stories.js
@@ -1,86 +1,162 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect,
+  useMemo,
+  useCallback,
+  useRef,
+} from 'react';
 import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { Button, Card } from 'antd';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 // import { withKnobs } from '@storybook/addon-knobs/react';
+import Timer from '../components/Timer';
 import RouteTabs from '../components/RouteTabs';
 
 const parentPath = '/parent';
 
-const China = () => {
-  return <div>this is China component</div>;
+const Country = ({ name, onIncrease }) => {
+  const onIncreaseRef = useRef();
+
+  useEffect(() => {
+    onIncreaseRef.current = onIncrease;
+  }, [onIncrease]);
+
+  useEffect(() => {
+    console.log(`${name} mounted`);
+
+    return () => {
+      console.log(`${name} unmounted`);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div style={{ marginBottom: '10px' }}>this is {name} component</div>
+
+      <Button type="primary" onClick={onIncreaseRef.current}>
+        数量改变
+      </Button>
+    </div>
+  );
 };
 
-const Japan = () => {
-  return <div>this is Japan component</div>;
-};
+const StandardDemo = () => {
+  const [now, setNow] = useState(Date.now());
+  const [tabs, setTabs] = useState([]);
+  const [counts, setCounts] = useState({});
+  const onIncreaseRef = useRef();
 
-const Korea = () => {
-  return <div>this is Korea component</div>;
-};
+  useEffect(() => {
+    onIncreaseRef.current = (name) => {
+      const curCount = counts[name] || 0;
+      setCounts({
+        ...counts,
+        [name]: curCount + 1,
+      });
+    };
+  }, [counts]);
 
-const Singapore = () => {
-  return <div>this is Singapore component</div>;
-};
+  const createCom = useCallback((name) => {
+    const Com = () => {
+      return (
+        <Country
+          name={name}
+          onIncrease={() => {
+            if (onIncreaseRef.current) {
+              onIncreaseRef.current(name);
+            }
+          }}
+        />
+      );
+    };
+    Com.displayName = `${name}`;
 
-const Malaysia = () => {
-  return <div>this is Malaysia component</div>;
-};
+    return Com;
+  }, []);
 
-const tabs = [
-  {
-    title: '中国',
-    path: 'china',
-    component: China,
-  },
-  {
-    title: '日本',
-    path: 'japan',
-    component: Japan,
-  },
-  {
-    title: '韩国',
-    path: 'korea',
-    component: Korea,
-  },
-  {
-    title: '新加坡',
-    path: 'singapore',
-    component: Singapore,
-  },
-  {
-    title: '马来西亚',
-    path: 'malaysia',
-    component: Malaysia,
-  },
-];
+  const China = useMemo(() => {
+    return createCom('china');
+  }, [createCom]);
+
+  const Japan = useMemo(() => {
+    return createCom('japan');
+  }, [createCom]);
+
+  const Korea = useMemo(() => {
+    return createCom('korea');
+  }, [createCom]);
+
+  useEffect(() => {
+    setTabs([
+      {
+        title: '中国',
+        path: 'china',
+        component: China,
+      },
+      {
+        title: '日本',
+        path: 'japan',
+        component: Japan,
+      },
+      {
+        title: '韩国',
+        path: 'korea',
+        component: Korea,
+      },
+    ]);
+  }, [China, Japan, Korea]);
+
+  const tabs2use = useMemo(() => {
+    return tabs.map((t) => {
+      return {
+        ...t,
+        title: `${t.title}(${counts[t.path] || 0})`,
+      };
+    });
+  }, [tabs, counts]);
+
+  return (
+    <Router>
+      <Timer
+        interval={1000}
+        onElapsed={() => {
+          setNow(Date.now());
+        }}
+      />
+      <div
+        style={{
+          background: '#F6F9FC',
+          padding: '20px',
+        }}
+      >
+        <Link to={parentPath}>
+          <Button type="primary">
+            打开新标签页测试。可在浏览器地址输入栏查看路由的变化
+          </Button>
+        </Link>
+      </div>
+      <div style={{ padding: '20px' }}>
+        <div>为了避免循环渲染，请确保component不变。</div>
+        <div
+          style={{
+            padding: '10px 0 0 0',
+          }}
+        >
+          now:{now}
+        </div>
+      </div>
+      <Switch>
+        <Route path={parentPath}>
+          <Card>
+            <RouteTabs tabs={tabs2use} />
+          </Card>
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 storiesOf('RouteTabs', module)
   // .addDecorator(withKnobs)
-  .add('标准', () => {
-    return (
-      <Router>
-        <div
-          style={{
-            margin: '0 0 50px 0',
-            background: '#F6F9FC',
-            padding: '20px',
-          }}
-        >
-          <Link to={parentPath}>
-            <Button type="primary">
-              打开新标签页测试。可在浏览器地址输入栏查看路由的变化
-            </Button>
-          </Link>
-        </div>
-        <Switch>
-          <Route path={parentPath}>
-            <Card>
-              <RouteTabs tabs={tabs} />
-            </Card>
-          </Route>
-        </Switch>
-      </Router>
-    );
-  });
+  .add('标准', StandardDemo);
