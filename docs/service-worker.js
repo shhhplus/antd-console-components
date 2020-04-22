e60be29b6fbbf3c6f778a9eb18771125acaef3df
shhhplus <shhhplus@msn.com>
2020-04-22 21:19:03 +0800

    build storybook

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.c5d1607fe1baa7a3b65b7bff8b201e51.js"
+  "precache-manifest.e639caed16868598082794e9123ee510.js"
 );
 
 self.addEventListener('message', (event) => {
