72175f7e514babecda980d2dcf6d811044139a7e
shhhplus <shhhplus@msn.com>
2020-04-23 15:25:32 +0800

    update Drawer组件

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,51 +1,29 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
 import { Drawer } from 'antd';
 import styles from './index.module.scss';
 
 export default (props) => {
-  const containerRef = useRef();
-  const bodyOverflowRef = useRef(undefined);
-
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
     setVisible(props.visible);
   }, [props.visible]);
 
-  useEffect(() => {
-    if (visible) {
-      debugger;
-      document.body.style.overflow = 'hidden';
-    } else {
-      if (bodyOverflowRef.current !== undefined) {
-        debugger;
-        document.body.style.overflow = bodyOverflowRef.current;
-      }
-    }
-  }, [visible]);
-
-  useEffect(() => {
-    debugger;
-    bodyOverflowRef.current = document.body.style.overflow;
-  }, []);
-
-  const { width = 900, onClose, ...rest } = props;
+  const { width = 900, className, onClose, ...rest } = props;
 
   return (
-    <div ref={containerRef} className={styles['drawer']}>
-      <Drawer
-        {...rest}
-        title={null}
-        maskClosable={false}
-        width={width}
-        visible={visible}
-        afterVisibleChange={setVisible}
-        onClose={() => {
-          setVisible(false);
-          onClose && onClose();
-        }}
-        getContainer={() => containerRef.current}
-      />
-    </div>
+    <Drawer
+      {...rest}
+      className={classNames(styles['drawer'], className)}
+      title={null}
+      maskClosable={false}
+      width={width}
+      visible={visible}
+      onClose={() => {
+        setVisible(false);
+        onClose && onClose();
+      }}
+    />
   );
 };
