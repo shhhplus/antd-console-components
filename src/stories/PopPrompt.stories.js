b3abf192da677a314bedc75b04234b0c5bcb28f6
shhhplus <shhhplus@msn.com>
2020-04-28 18:21:24 +0800

    rename EnhancedPopover to PopPrompt

--- src/stories/PopPrompt.stories.js
+++ src/stories/PopPrompt.stories.js
@@ -1,9 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Space } from 'antd';
 import { storiesOf } from '@storybook/react';
-import EnhancedPopover from '../components/EnhancedPopover';
+import PopPrompt from '../components/PopPrompt';
 
 const StandardDemo = (props) => {
+  const [visible, setVisible] = useState(false);
+
+  const actions = (
+    <Space>
+      <Button
+        type="primary"
+        size="small"
+        onClick={() => {
+          console.log('submit.');
+          setVisible(false);
+        }}
+      >
+        我知道了
+      </Button>
+    </Space>
+  );
+
+  return (
+    <div style={{ width: '300px', margin: '200px auto' }}>
+      <PopPrompt
+        title="信息标题"
+        desc={
+          <div style={{ maxWidth: '220px' }}>点击下方按钮可关闭弹出框！</div>
+        }
+        placement="topRight"
+        visible={visible}
+        onVisibleChange={setVisible}
+        actions={actions}
+      >
+        <Button type="link" style={{ paddingLeft: 0, paddingRight: 0 }}>
+          查看提示信息
+        </Button>
+      </PopPrompt>
+    </div>
+  );
+};
+
+const PopconfirmDemo = (props) => {
   const [visible, setVisible] = useState(false);
   const [processing, setProcessing] = useState(false);
 
@@ -40,7 +78,7 @@ const StandardDemo = (props) => {
 
   return (
     <div style={{ width: '300px', margin: '200px auto' }}>
-      <EnhancedPopover
+      <PopPrompt
         title="确认删除该手机号吗？"
         desc={
           <div style={{ maxWidth: '220px' }}>
@@ -51,13 +89,16 @@ const StandardDemo = (props) => {
         visible={visible}
         onVisibleChange={setVisible}
         actions={actions}
+        arrowPointAtCenter={true}
       >
         <Button type="link" style={{ paddingLeft: 0, paddingRight: 0 }}>
           重启
         </Button>
-      </EnhancedPopover>
+      </PopPrompt>
     </div>
   );
 };
 
-storiesOf('EnhancedPopover', module).add('标准', StandardDemo);
+storiesOf('PopPrompt', module)
+  .add('标准', StandardDemo)
+  .add('确认框', PopconfirmDemo);
